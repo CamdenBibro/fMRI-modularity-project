eadc6b9868a5de2eeae160faef957c389075ebd6
CamdenBibro <bibrocamden@gmail.com>
2023-10-20 15:43:29 -0500

    patients_v1.0

--- Community_Louvain_Vigilance.m
+++ Community_Louvain_Vigilance.m
@@ -1,25 +1,18 @@
 close all
 clear all
 
-addpath Z:\shared_toolboxes\Derek_functions\
-addpath Z:\shared_toolboxes\MatlabProgressBar\
-addpath Z:\shared_toolboxes\2019_03_03_BCT\
-load("Z:\Camden\fMRI_data\Vigilance_max_min\con_data_max.mat");
-max_vig_data = permute(con_data_max, [2,3,1]);
-load("Z:\Camden\fMRI_data\Vigilance_max_min\con_data_low.mat");
-min_vig_data = permute(con_data, [2,3,1]);
-load("Z:\000_Data\fMRI\spm8_new_preprocessed_data\data\parcellated_and_connectivity_fmri_data\AAN_Bfore_BNST_CONN_2minWindow_2sStride\pat20\pat20_parcellated.mat")
+addpath C:\Users\bibroce\OneDrive - VUMC\MATLAB\CEB-helper-functions\
+addpath "C:\Users\bibroce\OneDrive - VUMC\MATLAB\CEB-helper-functions\BrainNetViewer_20191031\"
+addpath "C:\Users\bibroce\OneDrive - VUMC\MATLAB\CEB-helper-functions\2019_03_03_BCT\"
+load("C:\Users\bibroce\OneDrive - VUMC\MATLAB\fMRI_data\Vigilance_max_min\pat_data_low.mat");
+min_vig_data = permute(pat_data, [2,3,1]);
+load("C:\Users\bibroce\OneDrive - VUMC\MATLAB\fMRI_data\Vigilance_max_min\pat_data_max.mat");
+max_vig_data = permute(pat_data_max, [2,3,1]);
+load("C:\Users\bibroce\OneDrive - VUMC\MATLAB\fMRI_data\pat20_parcellated.mat")
 network_names = fMRI_struct.region_names_ic;
 net_idx = fMRI_struct.node_network_ids;
 [~,net_sorted_idx] = sort(net_idx);
-x = 2; 
-% 
-% 
-% 
-% patient_data =  permute(patient_data, [2,3,1]);
-% patient_data(:,:,10) = [];        % remove problematic subjects
-% control_data = permute(control_data, [2,3,1]); 
-% control_data(:,:,[37,5,12]) = []; % remove problematic subjects
+
 
 %% max vig
 n = 129;
@@ -58,8 +51,8 @@ NMI(NMI==1) = 0;
 %figure, imagesc(NMI)
 figure, plot(mean(NMI)); title("MIN VIG: mean NMI")
 
-M2_con_max = M2_max == M2_max';
-M3_con = M3 == M3';
+M2_pat_max = M2_max == M2_max';
+M3_pat = M3 == M3';
 
 reordered_NMI = zeros(size(NMI));
 reordered_M2_max = M2_max == M2_max'; 
@@ -69,20 +62,20 @@ reordered_M3 = zeros(size(NMI));
 [~,idx] = sort(M2_max);
 for i = 1:129
     for j = 1:129
-        reordered_M2_max(j,i) = M2_con_max(idx(j), idx(i)); 
+        reordered_M2_max(j,i) = M2_pat_max(idx(j), idx(i)); 
         reordered_NMI(j,i) = NMI(idx(j), idx(i)); 
         reordered_W3(j,i) = W3(idx(j), idx(i));
         reordered_W2(j,i) = W2(idx(j), idx(i));
-        reordered_M3(j,i) = M3_con(idx(j), idx(i));
+        reordered_M3(j,i) = M3_pat(idx(j), idx(i));
     end
 end
 % for i = 1:129
 %     for j = 1:129
-%         reordered_M2(j,i) = M2_con(net_sorted_idx(j), net_sorted_idx(i)); 
+%         reordered_M2(j,i) = M2_pat(net_sorted_idx(j), net_sorted_idx(i)); 
 %         reordered_NMI(j,i) = NMI(net_sorted_idx(j), net_sorted_idx(i)); 
 %         reordered_W3(j,i) = W3(net_sorted_idx(j), net_sorted_idx(i));
 %         reordered_W2(j,i) = W2(net_sorted_idx(j), net_sorted_idx(i));
-%         reordered_M3(j,i) = M3_con(net_sorted_idx(j), net_sorted_idx(i));
+%         reordered_M3(j,i) = M3_pat(net_sorted_idx(j), net_sorted_idx(i));
 %     end
 % end
 figure, imagesc(reordered_M2_max); title("MAX VIG: reordered mod outputs (not normalized)"); xticks([0:129]); xticklabels(network_names(idx)); yticks([0:129]); yticklabels(network_names(idx))
@@ -93,7 +86,7 @@ figure, imagesc(reordered_NMI, "CDataMapping","scaled"); title("max VIG: reorder
 figure, imagesc(reordered_W2, "CDataMapping","scaled"); title("max VIG: reordered association mat from K3 (not normalized)"); colorbar
 %figure, imagesc(reordered_W3); title("reordered association mat from K3 (normalized)")
 
-%% Control Louvain %%
+%% pattrol Louvain %%
 n = 129;
 pat_data = min_vig_data; % make smaller dataset for testing
 [pat] = size(pat_data,3); 
@@ -121,17 +114,17 @@ end
  W3 = weight_conversion(W2, 'normalize');
 
  % louvain on association matrix %
-[M2_min, q1_con] = consensus_community_louvain_with_finetuning(W2, gamma1);
+[M2_min, q1_pat] = consensus_community_louvain_with_finetuning(W2, gamma1);
 [M3, ~] = consensus_community_louvain_with_finetuning(W3, gamma1); 
 
 %% 
 [~, NMI] = partition_distance(K2');
 NMI(NMI==1) = 0;
 figure, imagesc(NMI)
-figure, plot(mean(NMI)); title("controls mean NMI")
+figure, plot(mean(NMI)); title("pattrols mean NMI")
 
-M2_con_min = M2_min == M2_min';
-M3_con = M3 == M3';
+M2_pat_min = M2_min == M2_min';
+M3_pat = M3 == M3';
 
 reordered_NMI = zeros(size(NMI));
 reordered_M2_min = M2_min == M2_min'; 
@@ -141,20 +134,20 @@ reordered_M3 = zeros(size(NMI));
 [~,idx] = sort(M2_min);
 for i = 1:129
     for j = 1:129
-        reordered_M2_min(j,i) = M2_con_min(idx(j), idx(i)); 
+        reordered_M2_min(j,i) = M2_pat_min(idx(j), idx(i)); 
         reordered_NMI(j,i) = NMI(idx(j), idx(i)); 
         reordered_W3(j,i) = W3(idx(j), idx(i));
         reordered_W2(j,i) = W2(idx(j), idx(i));
-        reordered_M3(j,i) = M3_con(idx(j), idx(i));
+        reordered_M3(j,i) = M3_pat(idx(j), idx(i));
     end
 end
 % for i = 1:129
 %     for j = 1:129
-%         reordered_M2(j,i) = M2_con(net_sorted_idx(j), net_sorted_idx(i)); 
+%         reordered_M2(j,i) = M2_pat(net_sorted_idx(j), net_sorted_idx(i)); 
 %         reordered_NMI(j,i) = NMI(net_sorted_idx(j), net_sorted_idx(i)); 
 %         reordered_W3(j,i) = W3(net_sorted_idx(j), net_sorted_idx(i));
 %         reordered_W2(j,i) = W2(net_sorted_idx(j), net_sorted_idx(i));
-%         reordered_M3(j,i) = M3_con(net_sorted_idx(j), net_sorted_idx(i));
+%         reordered_M3(j,i) = M3_pat(net_sorted_idx(j), net_sorted_idx(i));
 %     end
 % end
 
